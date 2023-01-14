28e3d3c20f1861a8fb28024d7f2732ab8eddc60e
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-01-14 23:22:26 +0330

    "Requesting Data from API"

--- HackerNews-clone/pages/stories.js
+++ HackerNews-clone/pages/stories.js
@@ -1,5 +1,25 @@
 import view from "../utils/view";
 
-export default function Stories(path) {
+export default async function Stories(path) {
+  const stories = await getStories(path);
+  const hasStories = stories.length > 0;
+
+  view.innerHTML = `<div>
+  ${hasStories ? stories.map((story) => JSON.stringify(story)) : "No stories"}
+  </div>`;
+
   view.innerHTML = `<div>${path}</div>`;
+}
+
+async function getStories(path) {
+  const isHomeRoute = path === "/";
+  const isNewRoute = path === "/new";
+  if (isHomeRoute) {
+    path = "/news";
+  } else if (isNewRoute) {
+    path = "/newest ";
+  }
+  const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+  const stories = await response.json();
+  return stories;
 }
