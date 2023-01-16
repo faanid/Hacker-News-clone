0ca5bad9abbbd6a109d839d97624b953a0a974b5
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-01-16 12:51:56 +0330

    Formatting Stories

---
+++ components/Story.js
@@ -0,0 +1,26 @@
+export default function Story(story) {
+  //   console.log(story);
+  return `
+  <div class="story">
+    <div> 
+      <span class="gray">${story.index}</span>
+      <span class="upvote">▲</span>
+    <a href="${story.url}">${story.title}</a>
+    <span>(${story.domain}</span>
+    </div>
+    <div>
+    <div class="gray">
+    ${story.point} point by ${story.user} ${story.time_ago}
+    |
+    <a href="#/item?id=${story.id}">${story.comments_count} comments </a>
+    |
+    <span class="favorite">
+    <img class="heart" src="https://icon.now.sh/heart/ccc">
+    Add To Favorites
+    </span>
+    </div>
+    </div>
+  </div>
+      
+      `;
+}
