ca8a39f24584b904a8372822843e87ef0a879f0a
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-01-31 19:56:34 +0330

    fix error 1/2

---
+++ pages/favorites.js
@@ -0,0 +1,36 @@
+import view from "../utils/view.js";
+import checkFavorite from "../../../../Downloads/000/cV8wvgfq/utils/checkFavorite.js";
+import store from "../store.js";
+import Story from "../components/Story.js";
+
+export default function Favorites() {
+  const { favorites } = store.getState();
+  const hasFavorites = favorites.length > 0;
+
+  view.innerHTML = `<div>
+    ${
+      hasFavorites
+        ? favorites
+            .map((story) =>
+              Story({
+                ...story,
+                isFavorite: checkFavorite(favorites, story),
+              })
+            )
+            .join("")
+        : "Add some favorites!"
+    }
+  </div>`;
+
+  document.querySelectorAll(".favorite").forEach((favoriteButton) => {
+    favoriteButton.addEventListener("click", function () {
+      const story = JSON.parse(this.dataset.story);
+      const isFavorited = checkFavorite(favorites, story);
+      store.dispatch({
+        type: isFavorited ? "REMOVE_FAVORITE" : "ADD_FAVORITE",
+        payload: { favorite: story },
+      });
+      Favorites();
+    });
+  });
+}
